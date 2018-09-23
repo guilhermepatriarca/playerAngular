00d560b5ec5b14b3d69704b50fffd315b3e59dcb
gpatriarca <guilherme.patriarca@xseed.pt>
2018-09-23 11:57:48 +0100

    music services

---
+++ src/services/musicData.js
@@ -0,0 +1,72 @@
+playerApp.factory('musicData', ($resource, $http) => {
+
+    const resourcesSongs = $resource('https://songs-api-ubiwhere.now.sh/api/songs');
+    const resourcesRegister = $resource('https://songs-api-ubiwhere.now.sh/api/auth/register');
+    const resourcesLogin = $resource('https://songs-api-ubiwhere.now.sh/api/auth/login');
+    let userStatus = false;
+    let token = null;
+
+    return {
+        getMusics: () => {
+            return resourcesSongs.query();
+        },
+        getMusic: id => {
+            return resourcesSongs.query({
+                id: id
+            })
+        },
+        registerUser: user => {
+            return resourcesRegister.save(user)
+        },
+        loginUser: user => {
+            return resourcesLogin.save({
+                email: user.email,
+                password: user.password
+            })
+        },
+        changeUserStatus: istoken => {
+            userStatus = !userStatus;
+            userStatus === true ? token = istoken : token = null;
+        },
+        getUserStatus: () => {
+            return userStatus;
+        },
+        setUser: () => {
+            return $http.get('https://songs-api-ubiwhere.now.sh/api/users/me', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+        },
+        getFavorite: (musicId) => {
+            return $http.get('https://songs-api-ubiwhere.now.sh/api/user-favorites/', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+        },
+        setFavorite: (musicId) => {
+            return $http.post('https://songs-api-ubiwhere.now.sh/api/user-favorites/', {
+                songId: musicId
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+        },
+        RemoveFavorite: (musicId) => {
+            return $http({
+                method: "DELETE",
+                url: "https://songs-api-ubiwhere.now.sh/api/user-favorites/",
+                   data: {
+                        songId:musicId
+                    }
+                ,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+        }
+    }
+})
