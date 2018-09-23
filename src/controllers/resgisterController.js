ab503dcdcc55c7aa0315d8876c66dc196c391fa8
gpatriarca <guilherme.patriarca@xseed.pt>
2018-09-23 11:59:34 +0100

    add controller register

---
+++ src/controllers/resgisterController.js
@@ -0,0 +1,22 @@
+'use strict';
+
+playerApp.controller('RegisterController',
+ function registerController($scope, musicData){
+
+    $scope.showPassword = false;
+    $scope.registerDone = false;
+    $scope.erroRegister = false;
+
+    $scope.create = user =>{
+        musicData.registerUser()
+        .$promise.then((data)=>{
+            $scope.registerDone = true;
+        })
+        .catch((response)=>{
+            console.log(response)
+            $scope.erroRegister = true;
+        })
+    }
+
+
+});
