c7c2908243d03ead4ae05a225dd223af73d27de7
gpatriarca <guilherme.patriarca@xseed.pt>
2018-09-23 11:57:20 +0100

    add list controller

---
+++ src/controllers/listController.js
@@ -0,0 +1,63 @@
+'use strict';
+
+playerApp.controller('MusicController',
+  function loginController($scope, musicData,$location,$filter) {
+
+let currentUserId =null;
+let objs;
+
+    $scope.userLogin = musicData.getUserStatus();
+    musicData.setUser().then((data) => {
+      currentUserId = data.id;
+  });
+
+  $scope.moreInformation= id =>{
+    $location.path(`/music${id}`);
+  }
+
+  $scope.addToFavorites = musicId =>{
+    let find = $filter('filter')(objs, {id: musicId}, true);
+    if(!find[0].favorite){
+      musicData.setFavorite(musicId).then((data)=>{
+        find[0].favorite = true;
+        $scope.musics =objs;
+      }).catch((response)=>{
+      })
+    }else{
+      musicData.RemoveFavorite(musicId)
+      .then((data)=>{
+          find[0].favorite = false;
+          $scope.musics =objs;
+      }).
+      catch((response)=>{
+
+      })
+    }
+    
+  }
+
+    musicData.getMusics()
+      .$promise.then((data) => {
+        objs = data;
+        for (const iterator of objs) {
+          iterator.favorite = false;
+        }
+        musicData.getFavorite().then((data)=>{
+          for (const item of data.data) {
+            let find = $filter('filter')(objs, {id: item.songId}, true);
+            try {
+            find[0].favorite = true;
+            }catch{
+      
+            }
+          }
+          $scope.musics =objs;
+        }).catch((response)=>{
+          $scope.musics =objs;
+        })
+      
+      })
+      .catch((response) => {
+        console.log(`Error: ${response}`)
+      })
+  });
